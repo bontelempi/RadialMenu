9f6a43a037e31061bb59957a8bfaa60c47900f1c
Jake Humphrey <jakematthewh@gmail.com>
2026-03-16 18:15:09 +1100

    default config updated

--- src/main/kotlin/com/bontelempi/radialmenu/config/ConfigManager.kt
+++ src/main/kotlin/com/bontelempi/radialmenu/config/ConfigManager.kt
@@ -64,15 +64,45 @@ object ConfigManager {
                 label = "Warps",
                 iconItem = "minecraft:ender_pearl",
                 children = mutableListOf(
-                    MenuItem(newId(), "Hub", "minecraft:grass_block", "warp hub"),
-                    MenuItem(newId(), "Dungeon Hub", "minecraft:skeleton_skull", "warp dungeon_hub"),
-                    MenuItem(newId(), "Park", "minecraft:oak_sapling", "warp park"),
-                    MenuItem(newId(), "Farming Islands", "minecraft:wheat", "warp farming_1"),
+                    MenuItem(newId(), "Hub",         "minecraft:grass_block", "warp hub"),
+                    MenuItem(newId(), "Garden",      "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjQ4ODBkMmMxZTdiODZlODc1MjJlMjA4ODI2NTZmNDViYWZkNDJmOTQ5MzJiMmM1ZTBkNmVjYWE0OTBjYjRjIn19fQ==", "warp garden"),
+                    MenuItem(newId(), "Dungeon Hub", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWI3MzhmYTA1YjI3ZjE4ODc0ZTU3ZDk1YjBiNmM5ZmFkZDFiNTU4ODk0ZjgyZTAyNjFiYTE1NDRlNmZiYmYxZCJ9fX0=", "warp dungeon_hub"),
+                    MenuItem(newId(), "Park",        "minecraft:oak_sapling", "warp park"),
                 )
             ),
-            MenuItem(newId(), "Skyblock Menu", "minecraft:nether_star", "sbmenu"),
-            MenuItem(newId(), "Warp Home", "minecraft:red_bed", "warp home"),
-            MenuItem(newId(), "Co-op", "minecraft:player_head", "coop"),
+            MenuItem(newId(), "Island",  "minecraft:red_bed",    "warp home"),
+            MenuItem(
+                id = newId(),
+                label = "Party Commands",
+                iconItem = "minecraft:player_head",
+                children = mutableListOf(
+                    MenuItem(newId(), "Disband",        "minecraft:tnt",       "p disband"),
+                    MenuItem(newId(), "Leave",          "minecraft:barrier",   "p leave"),
+                    MenuItem(newId(), "Say \"r\"",      "minecraft:lime_wool", "pc r"),
+                    MenuItem(newId(), "Say \"!dt arrows\"", "minecraft:arrow", "pc !dt arrows"),
+                )
+            ),
+            MenuItem(newId(), "Storage", "minecraft:chest",      "storage"),
+            MenuItem(
+                id = newId(),
+                label = "Call",
+                iconItem = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTY3Mzk3ODNkZDlhYjBjNDczMGYzYjRlZDU3N2U5NGFhM2ZiOTEyNDZjYWI5MmU1YmU0ZmEyZTVjNWQyNGE4NCJ9fX0=",
+                children = mutableListOf(
+                    MenuItem(newId(), "Maddox",  "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTMzNmQ3Y2M5NWNiZjY2ODlmNWU4Yzk1NDI5NGVjOGQxZWZjNDk0YTQwMzEzMjViYjQyN2JjODFkNTZhNDg0ZCJ9fX0=", "call maddox"),
+                    MenuItem(newId(), "Jax",     "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDNkYWM3YzNjMWU2YzljNzA3Yjg1NWI4NDM3YjEzNWY5M2Y5M2Y5MmEzMmQ0MzAwODIzMmRmMjcxZjg1YmVhIn19fQ==", "call jax"),
+                    MenuItem(newId(), "Maxwell", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2I3Y2E3YmE2MzU3ZTRhY2E4NzQyNTA5YjQ3NDU1Mjk2N2VjMjA4YjZlY2Q5YzNiZWEyM2Y5N2Y4NDg1NzM1In19fQ==", "call maxwell"),
+                )
+            ),
+            MenuItem(
+                id = newId(),
+                label = "Inventory",
+                iconItem = "minecraft:ender_chest",
+                children = mutableListOf(
+                    MenuItem(newId(), "Wardrobe", "minecraft:netherite_chestplate", "wd"),
+                    MenuItem(newId(), "Pet",      "minecraft:bone",                 "pet"),
+                    MenuItem(newId(), "Bags",     "minecraft:bundle",               "bags"),
+                )
+            ),
         )
     )
 }
